f79fe22c79858de42218c40226757c2a3b08a149
ASHISH KUMAR PAL <106040189+Ashish-kumar-pal-1436@users.noreply.github.com>
2026-07-28 12:52:35 +0530

    update server configuration and add auth service; refactor database connection

--- backend/src/shared/db/db.js
+++ backend/src/shared/db/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import {DB_NAME} from '../../constant.js'
 
 
 const connectDB = async() =>{
      try {
-          const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
+          const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`)
           console.log("MONGODB CONNECTED PORT :", connectionInstance.connection.host)
 
      } catch (error) {
         console.error("MONGODB ERROR :", error)
+        process.exit(1)
      }
 }
 
